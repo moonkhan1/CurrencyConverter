4ee89670fad3cbdf46857156ee4d2109073803fa
moonkhan1 <ayxanhesenov3105@gmail.com>
2022-04-25 12:50:55 -0700

    100/55

--- action.js
+++ action.js
@@ -24,36 +24,59 @@ var op1 = document.querySelectorAll('.left-currencies');
 var op2 = document.querySelectorAll('.right-currencies');
     var con_from = document.querySelector(".from");
     var con_to = document.querySelector(".to");
-    fetch('https://api.exchangerate.host/latest?').then((data) => data.json())
-      .then((data) => {
-        display(data);
-      });
+    var from_p = document.querySelector('.from_p')
+    var to_p = document.querySelector('.to_p')
+    // fetch(url).then((data) => data.json())
+    //   .then((data) => {
+    //     display(data);
+    //   });
 
-      function display(data) {
-        const entries = Object.entries(data);
-        for (var i = 0; i < entries.length; i++) {
-          // buttons[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][0]}</option>`;
-          // buttons2[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
-          buttons2[0].innerHTML += `<value="${entries[4][1]["RUB"]}"`
-          buttons2[1].innerHTML += `<value="${entries[4][1]["USD"]}"`
-          buttons2[2].innerHTML += `<value="${entries[4][1]["EUR"]}"`
-          buttons2[3].innerHTML += `<value="${entries[4][1]["GBP"]}"`
+    //   function display(data) {
+    //     const entries = Object.entries(data);
+    //     for (var i = 0; i < entries.length; i++) {
+    //       // buttons[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][0]}</option>`;
+    //       // buttons2[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
+    //       buttons2[0].innerHTML += `<value="${entries[4][1]["RUB"]}"`
+    //       buttons2[1].innerHTML += `<value="${entries[4][1]["USD"]}"`
+    //       buttons2[2].innerHTML += `<value="${entries[4][1]["EUR"]}"`
+    //       buttons2[3].innerHTML += `<value="${entries[4][1]["GBP"]}"`
 
-          buttons[0].innerHTML += `<value="${entries[4][1]["RUB"]}"`
-          buttons[1].innerHTML += `<value="${entries[4][1]["USD"]}"`
-          buttons[2].innerHTML += `<value="${entries[4][1]["EUR"]}"`
-          buttons[3].innerHTML += `<value="${entries[4][1]["GBP"]}"`
-          console.log(entries[4][1])
+    //       buttons[0].innerHTML += `<value="${entries[4][1]["RUB"]}"`
+    //       buttons[1].innerHTML += `<value="${entries[4][1]["USD"]}"`
+    //       buttons[2].innerHTML += `<value="${entries[4][1]["EUR"]}"`
+    //       buttons[3].innerHTML += `<value="${entries[4][1]["GBP"]}"`
+    //       var rub = document.getElementById("rub")
+    //       var usd = document.getElementById("usd")
+    //       let a;
+    //       console.log(data)
+          fr.addEventListener('blur', _=>{            
+                  if(document.getElementById("rub").classList.contains("active")){
+                    fetch('https://api.exchangerate.host/latest?base=RUB').then((data) => data.json())
+                      .then((data) => {
+                        display(data);
+                        console.log(data)
+                      });
+                      
+                      function display(data) {
+                        const entries = Object.entries(data);
+                        for (var i = 0; i < entries.length; i++) {
+                          if(document.getElementById("usd").classList.contains("active")){
+                            to.value = fr.value * entries[4][1]["USD"]
+                            from_p.innerHTML = `1 RUB = ${entries[4][1]["USD"]} USD`
+                            to_p.innerHTML = `1 USD = ${entries[4][1]["USD"]}`
+
+
+               }
+              }
+            
+
+
+
+          // console.log(entries[4][1])
         }
-      }
+          
+        }
+          })
 
-var rub = document.getElementById("rub")
-var usd = document.getElementById("usd")
-let a = to.value;
-fr.addEventListener('blur', _=>{
-  if(document.getElementById("rub").click == true && document.getElementById("usd").click == true){
-    a = (fr.value * entries[4][1]["USD"])
-    to.innerHTML = `value=${a}`
-  }
-})
-console.log(a)
+      
+
