49117b767e26e37183675b8586b6d99e0c349287
moonkhan1 <94567332+moonkhan1@users.noreply.github.com>
2022-04-26 12:03:25 +0400

    Update action.js

--- action.js
+++ action.js
@@ -8,7 +8,7 @@ buttons.forEach(button => {
   button.addEventListener('click', () => {
     buttons.forEach(button => button.classList.remove('active'));
     button.classList.add('active');
-    });
+  });
 });
 
 var buttons2 = document.querySelectorAll('.b2');
@@ -16,67 +16,306 @@ buttons2.forEach(button => {
   button.addEventListener('click', () => {
     buttons2.forEach(button => button.classList.remove('active'));
     button.classList.add('active');
-    });
+  });
 });
-  
+
 
 var op1 = document.querySelectorAll('.left-currencies');
 var op2 = document.querySelectorAll('.right-currencies');
-    var con_from = document.querySelector(".from");
-    var con_to = document.querySelector(".to");
-    var from_p = document.querySelector('.from_p')
-    var to_p = document.querySelector('.to_p')
-    // fetch(url).then((data) => data.json())
-    //   .then((data) => {
-    //     display(data);
-    //   });
-
-    //   function display(data) {
-    //     const entries = Object.entries(data);
-    //     for (var i = 0; i < entries.length; i++) {
-    //       // buttons[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][0]}</option>`;
-    //       // buttons2[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
-    //       buttons2[0].innerHTML += `<value="${entries[4][1]["RUB"]}"`
-    //       buttons2[1].innerHTML += `<value="${entries[4][1]["USD"]}"`
-    //       buttons2[2].innerHTML += `<value="${entries[4][1]["EUR"]}"`
-    //       buttons2[3].innerHTML += `<value="${entries[4][1]["GBP"]}"`
-
-    //       buttons[0].innerHTML += `<value="${entries[4][1]["RUB"]}"`
-    //       buttons[1].innerHTML += `<value="${entries[4][1]["USD"]}"`
-    //       buttons[2].innerHTML += `<value="${entries[4][1]["EUR"]}"`
-    //       buttons[3].innerHTML += `<value="${entries[4][1]["GBP"]}"`
-    //       var rub = document.getElementById("rub")
-    //       var usd = document.getElementById("usd")
-    //       let a;
-    //       console.log(data)
-          fr.addEventListener('blur', _=>{            
-                  if(document.getElementById("rub").classList.contains("active")){
-                    fetch('https://api.exchangerate.host/latest?base=RUB').then((data) => data.json())
-                      .then((data) => {
-                        display(data);
-                        console.log(data)
-                      });
-                      
-                      function display(data) {
-                        const entries = Object.entries(data);
-                        for (var i = 0; i < entries.length; i++) {
-                          if(document.getElementById("usd").classList.contains("active")){
-                            to.value = fr.value * entries[4][1]["USD"]
-                            from_p.innerHTML = `1 RUB = ${entries[4][1]["USD"]} USD`
-                            to_p.innerHTML = `1 USD = ${entries[4][1]["USD"]}`
+var con_from = document.querySelector(".from");
+var con_to = document.querySelector(".to");
+var from_p = document.querySelector('.from_p')
+var to_p = document.querySelector('.to_p')
 
 
-               }
-              }
-            
+// FROM - RUBLE
+fr.addEventListener('keyup', (event) => {
+  fr.value = event.target.value.replace(',','.')
+  if(fr.value.match(/[a-zA-Z]+/g)){
+    event.preventDefault()
+  }
+
+  if (document.getElementById("rub").classList.contains("active")) {
+    fetch('https://api.exchangerate.host/latest?base=RUB').then(data => data.json())
+      .then(data => {
+        display(data);
+        console.log(data)
+
+        function display(data) {
+          var entries = Object.entries(data);
+          // for (var i = 0; i < entries.length; i++) {
+          if (document.getElementById("usd2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=USD').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                // for(var i =0; i<entries2.length;i++){
+                to.value = fr.value * entries[4][1]["USD"]
+                from_p.innerHTML = `1 RUB = ${entries[4][1]["USD"]} USD`
+                to_p.innerHTML = `1 USD = ${entries2[4][1]["RUB"]}RUB `
+              })
 
 
+          }
 
-          // console.log(entries[4][1])
+
+          if (document.getElementById("eur2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=EUR').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                // for(var i =0; i<entries2.length;i++){
+                to.value = fr.value * entries[4][1]["EUR"]
+                from_p.innerHTML = `1 RUB = ${entries[4][1]["EUR"]} EUR`
+                to_p.innerHTML = `1 EUR = ${entries2[4][1]["RUB"]} RUB `
+
+              })
+
+
+          }
+          if (document.getElementById("gbp2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=GBP').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                // for(var i =0; i<entries2.length;i++){
+                to.value = fr.value * entries[4][1]["GBP"]
+                from_p.innerHTML = `1 RUB = ${entries[4][1]["GBP"]} GBP`
+                to_p.innerHTML = `1 GBP = ${entries2[4][1]["RUB"]} RUB `
+
+              })
+
+
+          }
+          if (document.getElementById("rub2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=RUB').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                // for(var i =0; i<entries2.length;i++){
+                to.value = fr.value
+                from_p.innerHTML = `1 RUB = ${entries[4][1]["RUB"]} RUB`
+                to_p.innerHTML = `1 RUB = ${entries2[4][1]["RUB"]} RUB `
+
+              })
+
+
+          }
         }
-          
-        }
-          })
+      })
+  }
 
-      
+  // FROM - USD
+  if (document.getElementById("usd").classList.contains("active")) {
+    fetch('https://api.exchangerate.host/latest?base=USD').then(data => data.json())
+      .then(data => {
+        display(data);
+        console.log(data)
+
+
+        function display(data) {
+          var entries = Object.entries(data);
+          if (document.getElementById("usd2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=USD').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value
+                from_p.innerHTML = `1 USD = ${entries[4][1]["USD"]} USD`
+                to_p.innerHTML = `1 USD = ${entries2[4][1]["USD"]}USD `
+              })
+
+
+          }
+          if (document.getElementById("rub2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=RUB').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["RUB"]
+                from_p.innerHTML = `1 USD = ${entries[4][1]["RUB"]} RUB`
+                to_p.innerHTML = `1 RUB = ${entries2[4][1]["USD"]}USD `
+              })
+
+
+          }
+          if (document.getElementById("eur2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=EUR').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["EUR"]
+                from_p.innerHTML = `1 USD = ${entries[4][1]["EUR"]} EUR`
+                to_p.innerHTML = `1 EUR = ${entries2[4][1]["USD"]}USD `
+              })
+
+
+          }
+          if (document.getElementById("gbp2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=GBP').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["GBP"]
+                from_p.innerHTML = `1 USD = ${entries[4][1]["GBP"]} GBP`
+                to_p.innerHTML = `1 GBP = ${entries2[4][1]["USD"]}USD `
+              })
+
+
+          }
+
+
+
+        }
+
+
+
+
+      }
+      )
+  }
+
+
+
+  // FROM - EUR
+  if (document.getElementById("eur").classList.contains("active")) {
+    fetch('https://api.exchangerate.host/latest?base=EUR').then(data => data.json())
+      .then(data => {
+        display(data);
+        console.log(data)
+
+
+        function display(data) {
+          var entries = Object.entries(data);
+          if (document.getElementById("eur2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=EUR').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value
+                from_p.innerHTML = `1 EUR = ${entries[4][1]["EUR"]} EUR`
+                to_p.innerHTML = `1 EUR = ${entries2[4][1]["EUR"]}EUR `
+              })
+
+
+          }
+          if (document.getElementById("rub2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=RUB').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["RUB"]
+                from_p.innerHTML = `1 EUR = ${entries[4][1]["RUB"]} RUB`
+                to_p.innerHTML = `1 RUB = ${entries2[4][1]["EUR"]}EUR `
+              })
+
+
+          }
+
+          if (document.getElementById("usd2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=USD').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["USD"]
+                from_p.innerHTML = `1 EUR = ${entries[4][1]["USD"]} USD`
+                to_p.innerHTML = `1 USD = ${entries2[4][1]["EUR"]}EUR `
+              })
+
+
+          }
+          if (document.getElementById("gbp2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=GBP').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["GBP"]
+                from_p.innerHTML = `1 EUR = ${entries[4][1]["GBP"]} GBP`
+                to_p.innerHTML = `1 GBP = ${entries2[4][1]["EUR"]}EUR `
+              })
+
+
+          }
+
+        }
+      }
+      )
+  }
+
+
+
+
+
+
+
+
+
+  //FROM - GBP
+  if (document.getElementById("gbp").classList.contains("active")) {
+    fetch('https://api.exchangerate.host/latest?base=GBP').then(data => data.json())
+      .then(data => {
+        display(data);
+        console.log(data)
+
+
+        function display(data) {
+          var entries = Object.entries(data);
+          if (document.getElementById("gbp2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=GBP').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value
+                from_p.innerHTML = `1 GBP = ${entries[4][1]["GBP"]} GBP`
+                to_p.innerHTML = `1 GBP = ${entries2[4][1]["GBP"]}GBP `
+              })
+
+
+          }
+          if (document.getElementById("rub2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=RUB').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["RUB"]
+                from_p.innerHTML = `1 GBP = ${entries[4][1]["RUB"]} RUB`
+                to_p.innerHTML = `1 RUB = ${entries2[4][1]["GBP"]}GBP `
+              })
+
+
+          }
+
+          if (document.getElementById("usd2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=USD').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["USD"]
+                from_p.innerHTML = `1 GBP = ${entries[4][1]["USD"]} USD`
+                to_p.innerHTML = `1 USD = ${entries2[4][1]["GBP"]}GBP `
+              })
+
+
+          }
+          if (document.getElementById("eur2").classList.contains("active")) {
+            fetch('https://api.exchangerate.host/latest?base=EUR').then(data2 => data2.json())
+              .then(data2 => {
+                console.log(data2)
+                var entries2 = Object.entries(data2);
+                to.value = fr.value * entries[4][1]["EUR"]
+                from_p.innerHTML = `1 GBP = ${entries[4][1]["EUR"]} EUR`
+                to_p.innerHTML = `1 EUR = ${entries2[4][1]["GBP"]}GBP `
+              })
+
+
+          }
+
+        }
+      }
+      )
+  }
+
+
+})
+
 
